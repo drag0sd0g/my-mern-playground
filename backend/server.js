3d4a14acacacc3d242448cd1f6e96fc45e3e47f2
Dragos Dogaru <dragosdog@gmail.com>
2022-12-17 17:22:28 +0900

    added goals controller and routing

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log("Hello World");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 7788;
+const { errorHandler } = require("./middleware/errorMiddleware");
+
+const app = express();
+
+//!!!Middleware needed to access req body!!!
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/goals", require("./routes/goalRoutes"));
+
+//overwrite default express err handler
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
